a389bec0e9015b66637ea90fa671b4a8f1c8bc3a
aespinozar19 <aespinozar19@gmail.com>
2022-12-16 01:54:16 -0500

    comentario

--- pasivoahorro_bk20221215/src/main/java/com/bootcamp/java/pasivoahorro/common/exceptionHandler/FunctionalException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.java.pasivoahorro.common.exceptionHandler;
-
-public class FunctionalException extends RuntimeException{
-    public FunctionalException(String messageError){
-        super(messageError);
-    }
-}
--- pasivoahorro_bk20221215/src/main/java/com/bootcamp/java/pasivoahorro/service/webClients/pasivoCuentaCorriente/WcPasivoCuentaCorrienteService.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.java.pasivoahorro.service.webClients.pasivoCuentaCorriente;
-
-import com.bootcamp.java.pasivoahorro.dto.ProductClientDTO;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
-public interface WcPasivoCuentaCorrienteService {
-    public Mono<ProductClientDTO> findByDocumentNumber(String DocumentNumber);
-}
