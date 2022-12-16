c15c2e4236caaa6589c22088eef5c108d75c176b
aespinozar19 <aespinozar19@gmail.com>
2022-12-15 23:18:24 -0500

    Se agrega el proyecto Producto Pasivo Cuenta Corriente

---
+++ pasivocuentacorriente/src/main/java/com/bootcamp/java/pasivocuentacorriente/PasivocuentacorrienteApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.java.pasivocuentacorriente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasivocuentacorrienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasivocuentacorrienteApplication.class, args);
+	}
+
+}
